49fd9c5918e7be51fc2ccf7a602a7269edcfa469
Shachar Ron Zohar <88171079+shacharRonZohar@users.noreply.github.com>
2023-04-05 16:16:07 +0300

    Added error handeling

--- chat-gebeta/chat.py
+++ chat-gebeta/chat.py
@@ -15,5 +15,44 @@ def welcome():
 
 @app.post('/chat')
 def chat():
-    message = request.json['message']
-    return {'data': query(message)}
+    input = validate_input(request)
+    if type(input) == dict:
+        return input, 400
+
+    try:
+        response = query(input)
+        return jsonify({'data': response})
+    except Exception as e:
+        internal_msg = f"Had an error while querying the api, type: {type(e).__name__}, {e.args}"
+        user_msg = 'Something went wrong while querying the api, please try again'
+        print(internal_msg)
+        return jsonify({'error': user_msg}), 500
+    # try:
+    #     message = request.json['message']
+    # except KeyError:
+    #     return {'error': 'Please provide a message'}, 400
+    # try:
+    #     validate_input(message)
+    # except ValueError as e:
+    #     return {'error': e.args[0]}, 400
+    # try:
+    #     return {'data': query(message)}
+    # except Exception as e:
+    #     internal_msg = f"Had an error while querying the api, type: {type(e).__name__}, {e.args}"
+    #     user_msg = 'Something went wrong while querying the api, please try again'
+    #     print(internal_msg)
+    #     return {'error': user_msg}, 500
+
+
+def validate_input(request):
+    if not request.is_json:
+        return {'error': 'Request must be a JSON request'}
+    message = request.get_json().get('message')
+    if not message:
+        return {'error': 'Please provide a message'}
+    if type(message) != str:
+        return {'error': 'Message must be a string'}
+    if len(message) > 1024:
+        return {'error': 'Message is too long'}
+
+    return message
--- chat-gebeta/services/gpt.py
+++ chat-gebeta/services/gpt.py
@@ -8,10 +8,9 @@ headers = {'Authorization': f"Bearer {environ['API_TOKEN']}"}
 
 
 def query(payload):
+    print(payload)
     # The api returns a list with a single dictionary, containing the generated text
     response = post(API_URL, headers=headers, json=payload).json()
-    raw_data = response[0]['generated_text']
-    # The generated text contains both the input and the output, so we need to split it
-    split_string = f"{payload}\n\n"
-    data = raw_data.split(split_string)[1]
+    print(response)
+    data = response[0]['generated_text']
     return {'user_input': payload, 'bot_response': data}
