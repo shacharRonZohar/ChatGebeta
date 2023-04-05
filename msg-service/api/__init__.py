71ab5d8d7e78231e1a76514a12cb3a31e34da5fd
Shachar Ron Zohar <88171079+shacharRonZohar@users.noreply.github.com>
2023-04-06 00:45:33 +0300

    Further changes to docker

---
+++ msg-service/api/__init__.py
@@ -0,0 +1,41 @@
+import os
+
+from flask import Flask
+
+from .chat import bp as chat_bp
+# import logging
+# from dotenv import load_dotenv
+
+
+def create_app(test_config=None):
+    # Create and configure the app.
+    #  - instance_relative_config=True tells the app that if there are configuration files relative to the instance folder, they should override the default configuration.
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        # A default secret key that should be overridden with a random value when deploying.
+        SECRET_KEY='dev',
+        # port
+    )
+
+    if test_config is None:
+        # Load the instance config, if it exists, when not testing.
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # Load the test config if passed in.
+        app.config.from_mapping(test_config)
+
+    # Ensure the instance folder exists.
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # logging.basicConfig(level=logging.DEBUG)
+    
+    @app.get('/')
+    def index():
+        return {'data': 'Welcome to ChatGebeta, a ChatGPT api wrapper'}
+    
+    app.register_blueprint(chat_bp, url_prefix='/api/chat')
+
+    return app
