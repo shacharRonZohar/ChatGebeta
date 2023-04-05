332c37527e7174a68a9cae3fd8d471d7cc651bd7
Shachar Ron Zohar <88171079+shacharRonZohar@users.noreply.github.com>
2023-04-06 02:53:00 +0300

    Further changes to docker

--- model-service/api/__init__.py
+++ model-service/api/__init__.py
@@ -7,22 +7,22 @@ from .model import bp as model_bp
 # from dotenv import load_dotenv
 
 
-def create_app(test_config=None):
+def create_app(config=None, import_name=__name__):
     # Create and configure the app.
     #  - instance_relative_config=True tells the app that if there are configuration files relative to the instance folder, they should override the default configuration.
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(import_name, instance_relative_config=True)
     app.config.from_mapping(
         # A default secret key that should be overridden with a random value when deploying.
         SECRET_KEY='dev',
         # port
     )
 
-    if test_config is None:
+    if config is None:
         # Load the instance config, if it exists, when not testing.
         app.config.from_pyfile('config.py', silent=True)
     else:
         # Load the test config if passed in.
-        app.config.from_mapping(test_config)
+        app.config.from_mapping(config)
 
     # Ensure the instance folder exists.
     try:
