dfc729df8631703d1626950ea42afc556898037d
Shachar Ron Zohar <88171079+shacharRonZohar@users.noreply.github.com>
2023-04-06 20:31:18 +0300

    Cleanup

--- model-service/api/__init__.py
+++ model-service/api/__init__.py
@@ -5,22 +5,26 @@ from flask import Flask
 
 from .model import bp as model_bp
 
-# Create and configure the app.
-#  - instance_relative_config=True tells the app that if there are configuration files relative to the instance folder, they should override the default configuration.
-app = Flask(__name__, instance_relative_config=True)
-app.config.from_mapping(
-    # A default secret key that should be overridden with a random value when deploying.
-    SECRET_KEY='dev',
-    DATABASE=os.path.join(app.instance_path, 'chat_gebeta.sqlite')
-)
 
-app.config.from_pyfile('config.py', silent=True)
+def create_app(test_config=None):
+    # Create and configure the app.
+    #  - instance_relative_config=True tells the app that if there are configuration files relative to the instance folder, they should override the default configuration.
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        # A default secret key that should be overridden with a random value when deploying.
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'chat_gebeta.sqlite')
+    )
 
-logging.basicConfig(filename="chat-gebeta.log", level=logging.DEBUG)
+    app.config.from_pyfile('config.py', silent=True)
 
-try:
-    os.makedirs(app.instance_path)
-except OSError:
-    pass
+    logging.basicConfig(filename="chat-gebeta.log", level=logging.DEBUG)
 
-app.register_blueprint(model_bp, url_prefix='/api/model')
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    app.register_blueprint(model_bp, url_prefix='/api/model')
+
+    return app
