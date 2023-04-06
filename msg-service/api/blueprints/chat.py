cc44942e2b36758b169f333ca0833b455f7eb1cb
Shachar Ron Zohar <88171079+shacharRonZohar@users.noreply.github.com>
2023-04-06 21:46:13 +0300

    Added dockerignore to msg service

--- msg-service/api/blueprints/chat.py
+++ msg-service/api/blueprints/chat.py
@@ -1,8 +1,12 @@
+import logging
+
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
+
 from ..services.gpt import query
-from ..services.logger import logger
+
+# from ..services.logger import logger
 
 from ..schemas.validation.main import validate_chat_input
 
@@ -16,14 +20,14 @@ def index():
 
 @bp.post('/chat')
 def chat():
-    logger.info('Request made to /chat, validating input')
+    logging.info('Request made to /chat, validating input')
     user_input = validate_chat_input(request)
     print(user_input)
     # The validate_input function returns a dictionary if the input is invalid
     if "error" in user_input:
-        logger.info('Input validation failed, returning error')
+        logging.info('Input validation failed, returning error')
         return user_input, 400
-    logger.info('Input validated, querying the model')
+    logging.info('Input validated, querying the model')
     try:
         print(user_input)
         response = query(user_input)
@@ -31,6 +35,6 @@ def chat():
     except Exception as e:
         # I wanted to log the error for internal use, while returning a user friendly error message
         internal_msg = f"Had an error while querying the model, type: {type(e).__name__}, {e.args}"
-        logger.exception(internal_msg)
+        logging.exception(internal_msg)
         user_msg = 'Something went wrong while querying the model, please try again'
         return {'error': user_msg}, 500
