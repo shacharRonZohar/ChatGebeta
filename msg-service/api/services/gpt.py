1a5fed3941ce46665917806571c670b13e1fdd6e
Shachar Ron Zohar <88171079+shacharRonZohar@users.noreply.github.com>
2023-04-06 15:38:30 +0300

    cleanup

--- msg-service/api/services/gpt.py
+++ msg-service/api/services/gpt.py
@@ -1,14 +1,9 @@
-from os import environ
 from requests import post
 
 # For local self hosted model
 # API_URL = 'http://localhost:5050/api/model/'
 # For self hosted model
 API_URL = 'https://chatgebeta-model-production.up.railway.app/api/model/'
-
-
-# headers = {'Authorization': f"Bearer {environ['API_TOKEN']}"}
-
 
 def query(payload):
     endpoint = 'generate'
