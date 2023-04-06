beaf53928156da1637db7da1c8e44b54fa572b26
Shachar Ron Zohar <88171079+shacharRonZohar@users.noreply.github.com>
2023-04-06 05:49:57 +0300

    change for ci test

--- msg-service/api/services/gpt.py
+++ msg-service/api/services/gpt.py
@@ -4,7 +4,6 @@ from requests import post
 # For local self hosted model
 # API_URL = 'http://localhost:5050/api/model/'
 # For self hosted model
-
 API_URL = 'https://chatgebeta-model-production.up.railway.app/api/model/'
 
 
