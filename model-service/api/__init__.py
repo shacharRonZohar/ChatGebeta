945bc06c66bd445bb7ba9a5d5a01c33f4c8c89f8
Shachar Ron Zohar <88171079+shacharRonZohar@users.noreply.github.com>
2023-04-06 04:27:58 +0300

    Changed url to hosted model, everything should be working in prod frfr

--- model-service/api/__init__.py
+++ model-service/api/__init__.py
@@ -3,9 +3,6 @@ import os
 from flask import Flask
 
 from .model import bp as model_bp
-# import logging
-# from dotenv import load_dotenv
-
 
 # Create and configure the app.
 #  - instance_relative_config=True tells the app that if there are configuration files relative to the instance folder, they should override the default configuration.
@@ -13,23 +10,12 @@ app = Flask(__name__, instance_relative_config=True)
 app.config.from_mapping(
     # A default secret key that should be overridden with a random value when deploying.
     SECRET_KEY='dev',
-    # port
 )
-# if config is None:
-# Load the instance config, if it exists, when not testing.
+
 app.config.from_pyfile('config.py', silent=True)
-# else:
-# Load the test config if passed in.
-# app.config.from_mapping(config)
-# Ensure the instance folder exists.
+
 try:
     os.makedirs(app.instance_path)
 except OSError:
     pass
-# logging.basicConfig(level=logging.DEBUG)
 app.register_blueprint(model_bp, url_prefix='/api/model')
-
-
-@app.get('/')
-def index():
-    return 'Hello, World!'
--- msg-service/api/services/gpt.py
+++ msg-service/api/services/gpt.py
@@ -1,7 +1,11 @@
 from os import environ
 from requests import post
 
-API_URL = 'http://localhost:5050/api/model/'
+# For local self hosted model
+# API_URL = 'http://localhost:5050/api/model/'
+# For self hosted model
+API_URL = 'https://chatgebeta-model-production.up.railway.app/api/model/'
+
 
 # headers = {'Authorization': f"Bearer {environ['API_TOKEN']}"}
 
